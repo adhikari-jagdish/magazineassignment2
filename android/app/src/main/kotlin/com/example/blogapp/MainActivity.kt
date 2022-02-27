809888786dc809fc5977240f945b147c5056168f
Jagdish <adhikari.jagdish5200@gmail.com>
2022-02-27 14:43:30 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/blogapp/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.blogapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
